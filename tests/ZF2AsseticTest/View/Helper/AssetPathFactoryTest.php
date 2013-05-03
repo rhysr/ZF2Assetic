9b42e99fe4265d148f4e076cd17cf0b0e8af5a7b
rhysr <rhysr@mega-city-one.co.uk>
2013-05-03 20:41:33 +0100

    Refactored asset view helper factory
    Means i can return different view helpers based on mode

--- tests/ZF2AsseticTest/View/Helper/AssetPathFactoryTest.php
+++ tests/ZF2AsseticTest/View/Helper/AssetPathFactoryTest.php
@@ -17,57 +17,69 @@ class AssetPathFactoryTest extends TestCase
 {
     private $factory;
 
-    private $serviceManager;
-
-    private $pluginManager;
-
     private $assetManager;
 
-    private $config = array(
+    private $router;
+
+    private $routeName;
+
+    private $controllerModeConfig = array(
         'zf2_assetic' => array(
             'controllerRouteName' => 'assetic',
         ),
     );
+
     public function setUp()
     {
         $this->factory        = new AssetPathFactory();
-        $this->serviceManager = new ServiceManager();
-        $this->pluginManager  = new HelperPluginManager();
         $this->assetManager   = new AssetManager();
         $this->router         = new TreeRouteStack();
         $this->routeName      = 'assetRoute';
-
-        $this->serviceManager->setService('AsseticAssetManager', $this->assetManager);
-        $this->serviceManager->setService('router', $this->router);
-        $this->pluginManager->setServiceLocator($this->serviceManager);
-
-        $this->serviceManager->setService('Configuration', $this->config);
-        $this->serviceManager->setAlias('Config', 'Configuration');
     }
 
     public function testFactoryCreatesViewHelper()
     {
-        $helper = $this->factory->createService($this->pluginManager);
+        $serviceManager = $this->createServiceManager($this->controllerModeConfig);
+        $pluginManager = $this->createPluginManager($serviceManager);
+
+        $helper = $this->factory->createService($pluginManager);
         $this->assertInstanceOf('\\ZF2Assetic\\View\\Helper\\AssetPath', $helper);
     }
 
     public function testRouteNameCorrectlySet()
     {
-        $helper = $this->factory->createService($this->pluginManager);
+        $serviceManager = $this->createServiceManager($this->controllerModeConfig);
+        $pluginManager = $this->createPluginManager($serviceManager);
+
+        $helper = $this->factory->createService($pluginManager);
         $this->assertEquals('assetic', $helper->getRouteName());
     }
 
     public function testThrowsExceptionIfControllerRouteNameNotSet()
     {
+        $serviceManager = $this->createServiceManager(array());
+        $pluginManager = $this->createPluginManager($serviceManager);
+
+        $this->setExpectedException('\\ZF2Assetic\\InvalidArgumentException');
+
+        $helper = $this->factory->createService($pluginManager);
+    }
+
+    public function createServiceManager($config)
+    {
         $serviceManager = new ServiceManager();
         $serviceManager->setService('AsseticAssetManager', $this->assetManager);
         $serviceManager->setService('router', $this->router);
-        $this->pluginManager->setServiceLocator($serviceManager);
-
-        $this->setExpectedException('\\ZF2Assetic\\InvalidArgumentException');
-        $serviceManager->setService('Configuration', array());
+        $serviceManager->setService('Configuration', $config);
         $serviceManager->setAlias('Config', 'Configuration');
-        $helper = $this->factory->createService($this->pluginManager);
+        return $serviceManager;
+    }
+
+    public function createPluginManager(ServiceManager $serviceManager)
+    {
+        $pluginManager  = new HelperPluginManager();
+        $pluginManager->setServiceLocator($serviceManager);
+        return $pluginManager;
     }
 }
 
